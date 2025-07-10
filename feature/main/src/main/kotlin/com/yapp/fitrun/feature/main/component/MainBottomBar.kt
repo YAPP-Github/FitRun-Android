db2fa4566ca7ea9f64ed20c93547a626b048c14d
pardessuccess <zkwhdgur238@gmail.com>
2025-07-10 21:08:41 +0900

    [FEAT][FITRUN-133][#25] Bottom Bar 설정

---
+++ feature/main/src/main/kotlin/com/yapp/fitrun/feature/main/component/MainBottomBar.kt
@@ -0,0 +1,116 @@
+package com.yapp.fitrun.feature.main.component
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import com.yapp.fitrun.core.designsystem.Caption3_semiBold
+import com.yapp.fitrun.feature.main.MainTab
+
+@Composable
+internal fun MainBottomBar(
+    modifier: Modifier = Modifier,
+    visible: Boolean,
+    tabs: List<MainTab>,
+    currentTab: MainTab?,
+    onTabSelected: (MainTab) -> Unit
+) {
+    AnimatedVisibility(
+        visible = visible,
+        enter = fadeIn() + slideIn { IntOffset(0, it.height) },
+        exit = fadeOut() + slideOut { IntOffset(0, it.height) }
+    ) {
+        Column {
+            Spacer(modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+                .background(Color(0xFFEEEEEE)))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(62.dp)
+            ) {
+                tabs.forEach { tab ->
+                    MainBottomBarItem(
+                        tab = tab,
+                        selected = tab == currentTab,
+                        onClick = { onTabSelected(tab) }
+                    )
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+private fun RowScope.MainBottomBarItem(
+    modifier: Modifier = Modifier,
+    tab: MainTab,
+    selected: Boolean,
+    onClick: () -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .selectable(
+                selected = selected,
+                indication = null,
+                role = null,
+                interactionSource = remember { MutableInteractionSource() },
+                onClick = onClick,
+            ),
+        contentAlignment = Alignment.Center,
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                painter = painterResource(tab.iconResId),
+                contentDescription = tab.name,
+                tint = if (selected) {
+                    tab.selectedColor
+                } else {
+                    tab.unselectedColor
+                },
+                modifier = Modifier.size(24.dp),
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = stringResource(tab.titleTextId),
+                style = Caption3_semiBold,
+                color = if (selected) {
+                    tab.selectedColor
+                } else {
+                    tab.unselectedColor
+                },
+            )
+        }
+    }
+
+}
