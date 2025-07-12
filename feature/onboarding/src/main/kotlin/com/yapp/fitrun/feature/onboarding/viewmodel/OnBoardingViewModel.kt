79c08f01223a3137fae068978f2b548a9d1b0208
josu_shell <yoy07030@khu.ac.kr>
2025-07-12 21:57:29 +0900

    [FEAT][FITRUN-34] :feature:onboarding API 연결

--- feature/onboarding/src/main/kotlin/com/yapp/fitrun/feature/onboarding/viewmodel/OnBoardingViewModel.kt
+++ feature/onboarding/src/main/kotlin/com/yapp/fitrun/feature/onboarding/viewmodel/OnBoardingViewModel.kt
@@ -1,6 +1,10 @@
 package com.yapp.fitrun.feature.onboarding.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.yapp.fitrun.core.domain.entity.OnBoardingEntity
+import com.yapp.fitrun.core.domain.repository.TokenRepository
+import com.yapp.fitrun.core.domain.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.viewmodel.container
@@ -11,9 +15,14 @@ import org.orbitmvi.orbit.syntax.simple.reduce
 
 @HiltViewModel
 class OnBoardingViewModel @Inject constructor(
-
+    private val userRepository: UserRepository,
+    private val tokenRepository: TokenRepository,
 ): ViewModel(), ContainerHost<OnBoardingState, OnBoardingSideEffect> {
     override val container = container<OnBoardingState, OnBoardingSideEffect>(OnBoardingState())
+
+    private val firstSelectedValue: List<String> = emptyList()
+    private val secondSelectedValue: List<String> = emptyList()
+    private val thirdSelectedValue: List<String> = emptyList()
 
     fun onClickOnBoardingFirst() = intent {
         val newCount: Int =  state.selectedOnBoardingFirstStateCount + 1
@@ -43,5 +52,27 @@ class OnBoardingViewModel @Inject constructor(
         if (newCount >= 2) {
             postSideEffect(OnBoardingSideEffect.NavigateToOnBoardingFourth)
         }
+    }
+
+    fun setOnBoardingInfo() = intent {
+        userRepository.setOnBoardingInfo(
+            onBoardingEntity = OnBoardingEntity(
+                answers =
+            )
+        )
+    }
+
+    fun getRunnerType() = intent {
+        reduce {
+            state.copy(isLoading = true)
+        }
+        userRepository.getUserRunnerType()
+            .onSuccess { response ->
+                reduce { state.copy(isLoading = false, runnerTypeResult = response.runnerType) }
+            }
+            .onFailure { e ->
+                reduce { state.copy(isLoading = false) }
+                Log.e(this@OnBoardingViewModel.javaClass.name, e.message.toString())
+            }
     }
 }
