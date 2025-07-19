c4d960bc8bf749a52ec6f9043cec62621d02e2c1
pardessuccess <zkwhdgur238@gmail.com>
2025-07-19 14:02:16 +0900

    [FEAT] 홈 API에 필요한 모델 설정

---
+++ core/domain/src/main/kotlin/com/yapp/fitrun/core/domain/entity/RecordEntity.kt
@@ -0,0 +1,9 @@
+package com.yapp.fitrun.core.domain.entity
+
+data class RecordEntity(
+    val recentDistanceMeter: Double? = null,
+    val recentPace: Int? = null,
+    val recentTime: Int? = null,
+    val thisWeekRunningCount: Int,
+    val totalDistance: Double
+)
