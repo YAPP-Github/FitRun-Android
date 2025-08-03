3ebf80af5508c0c6aeadbd3b89ea5947b61fab43
josushell <yoy07030@khu.ac.kr>
2025-08-03 13:55:36 +0900

    [FEAT][FITRUN-75] :feature:mypage 목적, 체력 수정 UI 구현

--- feature/mypage/src/main/java/com/yapp/fitrun/feature/mypage/MyPageNavigation.kt
+++ feature/mypage/src/main/java/com/yapp/fitrun/feature/mypage/MyPageNavigation.kt
@@ -5,22 +5,58 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import kotlinx.serialization.Serializable
 
 @Serializable
+data object MyPageSettingRoute
+
+@Serializable
 data object MyPageRoute
+
+@Serializable
+data object ChangeRunningLevelRoute
+
+@Serializable
+data object ChangeRunningPurposeRoute
 
 fun NavController.navigateToMyPage(navOptions: NavOptions) {
     navigate(MyPageRoute, navOptions)
 }
 
+fun NavController.navigateToChangeRunningLevelRoute() {
+    navigate(ChangeRunningLevelRoute)
+}
+
+fun NavController.navigateToChangeRunningPurposeRoute() {
+    navigate(ChangeRunningPurposeRoute)
+}
+
 fun NavGraphBuilder.myPageNavGraph(
     padding: PaddingValues,
 ) {
-    composable<MyPageRoute> {
-        MyPageRoute(
-            padding = padding,
-            // TODO: viewmodel
-        )
+    navigation<MyPageSettingRoute>(
+        startDestination = MyPageRoute,
+    ) {
+        composable<MyPageRoute> {
+            MyPageRoute(
+                padding = padding,
+                // TODO: viewmodel
+            )
+        }
+
+        composable<ChangeRunningLevelRoute> {
+            ChangeRunningLevelRoute(
+                padding = padding,
+                // TODO: viewmodel
+            )
+        }
+
+        composable<ChangeRunningPurposeRoute> {
+            ChangeRunningLevelRoute(
+                padding = padding,
+                // TODO: viewmodel
+            )
+        }
     }
 }
